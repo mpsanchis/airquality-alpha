f06ff3a532f376636871f373aabc0cd531d93822
Juan Jose Garau <juanjogarau@MacBook-Pro-de-Juan.local>
2018-01-20 13:11:38 +0100

    Join and Merge scripts

---
+++ cleaning/join.r
@@ -0,0 +1,44 @@
+# WORKING DIRECTORY ----------------------------------
+setwd("~/Documents/R/airquality-alpha")
+
+# LOAD PACKAGES --------------------------------------
+library(data.table)
+
+# LOAD DATA ------------------------------------------
+header_obs = data.table(read.csv("data/headers_obs.csv", sep="", stringsAsFactors=FALSE))
+header_mod = data.table(read.delim("data/headers_mod.csv", stringsAsFactors=FALSE))
+
+mod_files = list.files('data/mod', recursive=T)
+obs_files = list.files('data/obs', recursive=T)
+
+obs_data = copy(header_obs)
+mod_data = copy(header_mod)
+
+for (i in 1:length(obs_files)){
+  data = data.table(read.delim(paste0('data/obs/',obs_files[i]), header = F, stringsAsFactors = F))
+  names(data) = names(header_obs)
+  obs_data = rbind(obs_data, data)
+}
+
+for (i in 1:length(mod_files)){
+  data = data.table(read.table(paste0('data/mod/',mod_files[i]), quote="\"", comment.char="", stringsAsFactors=FALSE))
+  names(data) = names(header_mod)
+  if (i == 1){
+    mod_data = rbind(mod_data, data)
+    mod_data[,day:=as.Date(day)]
+    first_date = mod_data$day[1]
+    mod_data[,DayType := ifelse(day==first_date,'ConcentrationModSame','ConcentrationModPrev')]
+  } else {
+    data[,day:=as.Date(day)]
+    first_date = data$day[1]
+    data[,DayType := ifelse(day==first_date,'ConcentrationModSame','ConcentrationModPrev')]
+    mod_data = rbind(mod_data, data)
+  }
+}
+
+mod_data = dcast(mod_data, lon + lat + day + hour + AirPollutant ~ DayType, value.var = 'Concentration')
+mod_data = mod_data[,.(AirPollutant=AirPollutant[1],ConcentrationModPrev=sum(ConcentrationModPrev,na.rm=T),
+                       ConcentrationModSame=sum(ConcentrationModSame,na.rm=T)),by=.(day,hour,lat,lon)]
+
+write.csv(obs_data, 'data/obs_data.csv', row.names=F)
+write.csv(mod_data, 'data/mod_data.csv', row.names=F)
---
+++ cleaning/merge.r
@@ -0,0 +1,30 @@
+# WORKING DIRECTORY ----------------------------------
+setwd("~/Documents/R/airquality-alpha")
+
+# LOAD PACKAGES --------------------------------------
+library(data.table)
+
+# LOAD DATA ------------------------------------------
+obs_data = data.table(read.csv('data/obs_data.csv', stringsAsFactors = F))
+mod_data = data.table(read.csv('data/mod_data.csv', stringsAsFactors = F))
+stations = data.table(read.csv('data/stations.csv', stringsAsFactors = F))
+
+# MERGE DATA
+mod_data = merge(mod_data, stations[,.(code, lat, lon, height)], by=c('lat','lon'), all.x=T)
+obs_data[, DateBegin := sapply(DatetimeBegin, function(x){return(unlist(strsplit(x,' '))[1])})]
+obs_data[, TimeBegin := sapply(DatetimeBegin, function(x){return(unlist(strsplit(x,' '))[2])})]
+obs_data[, DateEnd := sapply(DatetimeEnd, function(x){return(unlist(strsplit(x,' '))[1])})]
+obs_data[, TimeEnd := sapply(DatetimeEnd, function(x){return(unlist(strsplit(x,' '))[2])})]
+
+setnames(obs_data, 'Concentration', 'ConcentrationObs')
+
+mod_data[,AirPollutant := NULL]
+
+data = merge(obs_data, mod_data, by.x=c('AirQualityStationEoICode','DateBegin','TimeBegin'), by.y=c('code','day','hour'),all.x=T)
+
+write.csv(data, 'data/full_data.csv', row.names = F)
+
+
+
+
+
